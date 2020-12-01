a66bf81fec3c8e3bb9db80ffe04789fa98676c74
Rudi Yardley <contact@rudiyardley.com>
2020-12-01 14:16:39 +1100

    --wip-- [skip ci]

--- scripts/_run_3.sh
+++ scripts/_run_3.sh
@@ -1,3 +1,8 @@
 . ./smart-contracts/.env
 
-ETHEREUM_PRIVATE_KEY=$ETHEREUM_PRIVATE_KEY ebrelayer init tcp://localhost:26657 ws://localhost:7545/ 0x30753E4A8aad7F8597332E813735Def5dD395028 user1 'race draft rival universe maid cheese steel logic crowd fork comic easy truth drift tomorrow eye buddy head time cash swing swift midnight borrow' --chain-id=sifchain
+ETHEREUM_PRIVATE_KEY=$ETHEREUM_PRIVATE_KEY ebrelayer init \
+  tcp://localhost:26657 \
+  ws://localhost:7545/ \
+  0x30753E4A8aad7F8597332E813735Def5dD395028 \
+  user1 "race draft rival universe maid cheese steel logic crowd fork comic easy truth drift tomorrow eye buddy head time cash swing swift midnight borrow" \
+  --chain-id=sifchain
